106472776c1926e55b39d2912a5f2832b309b432
Erica Obersi <evobersi@gmail.com>
2013-12-05 17:00:18 -0500

    working JSON for OMDAPI

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -35,7 +35,14 @@ var FormView = Backbone.View.extend({
   newMovie: function(e) {
     e.preventDefault();
     var title = this.$el.find("input[name='movie']").val();
-
+    $.ajax({
+    type: "GET",
+    url: "http://www.omdbapi.com/?s=" + title,
+    dataType: "json",
+    success: function(data){
+      console.log(data);
+    }
+  });
   }
 });
 
