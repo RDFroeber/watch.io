9e82163d0c51fbe75d15970a3360fa8bf7c252c8
Raleigh D Froeber <RDFroeber@gmail.com>
2013-12-05 15:36:50 -0500

    Movies and Model and View extended

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,29 @@
-app.js
+/*******
+* MODELS
+*******/
+
+var Movies = Backbone.Model.extend({
+  defaults: {
+    seen: false
+  }
+});
+
+
+/******
+* VIEWS
+******/
+
+var FormView = Backbone.View.extend({
+  el: "form",
+
+  events: {
+    "submit": "newMovie"
+  },
+
+  newMovie: function(e) {
+    e.preventDefault();
+    // var title = this.$el.find("input[name='movie']").val();
+  }
+});
+
+var form = new FormView();
