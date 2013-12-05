01a643fd53430b30940c17209f622b7d224b724b
Raleigh D Froeber <RDFroeber@gmail.com>
2013-12-05 16:03:14 -0500

    Layout application view reverted and application.js revised

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require underscore-min
-//= require backbone-min
+//= require underscore
+//= require backbone
 //= require turbolinks
 //= require_tree .
